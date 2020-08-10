c022f7b63d4db362d8988b95236c571fdc895733
Sahresh Waheed <sehrishwaheed98@gmail.com>
2020-08-10 23:28:33 +0300

    Updated moves

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Board from './components/Board';
+import History from './components/History';
 import { calculateWinner } from './helpers';
 import './styles/root.scss';
 
@@ -9,7 +10,7 @@ const App = () => {
   const [history, setHistory] = useState([
     { board: Array(9).fill(null), isXNext: true },
   ]);
-  console.log(history);
+
   const [currentMove, setCurrentMove] = useState(0);
   const current = history[currentMove];
 
@@ -38,11 +39,16 @@ const App = () => {
     setCurrentMove(prev => prev + 1);
   };
 
+  const moveTo = move => {
+    setCurrentMove(move);
+  };
+
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
       <h2>{message}</h2>
       <Board board={current.board} handleSquareClick={handleSquareClick} />
+      <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
 };
